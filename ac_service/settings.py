0e01b03a70d22efabecdf650f7a3abae0ea52c90
mrbabusir <pravinrokamagar86@gmail.com>
2026-03-23 13:12:42 +0545

    fix allowed hosts

--- ac_service/settings.py
+++ ac_service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'nepalairconservice.onrender.com').split(',')
 
 
 # Application definition

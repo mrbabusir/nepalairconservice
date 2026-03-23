c0e498612bd6be16a8a6ca34757a8989a08c7d45
mrbabusir <pravinrokamagar86@gmail.com>
2026-03-23 15:32:33 +0545

    fixeurls

--- ac_service/urls.py
+++ ac_service/urls.py
@@ -5,17 +5,6 @@ from django.conf.urls.static import static
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
-# ← functions must be defined BEFORE urlpatterns
-def create_super(request):
-    if not User.objects.filter(username='mrbabusir').exists():
-        User.objects.create_superuser(
-            username='mrbabusir',
-            password='NepAlAiRc0n@2025!',  # ← strong password
-            email='mrbabusir86@gmail.com'
-        )
-        return HttpResponse("Superuser created!")
-    return HttpResponse("Already exists!")
-
 admin.site.site_header = "Nepal Aircon Service"
 admin.site.site_title = "Nepal Aircon Admin"
 admin.site.index_title = "Management Panel"
@@ -26,7 +15,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('booking/', include('booking.urls')),
     path('accounts/', include('shop.urls')),
-    path('create-super/', create_super),
 ]
 
 if settings.DEBUG:
